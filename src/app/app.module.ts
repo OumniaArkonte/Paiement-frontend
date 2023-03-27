9691e5db817fbd36f639237795fbda7c9299a033
ORIGINAL SHOP <arkonteoumnia@gmail.com>
2023-03-27 23:05:42 +0000

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { PaiementCreateComponent } from './view/paiements/paiement-create/paiement-create.component';
-import { PaiementListComponent } from './view/paiements/paiement-list/paiement-list.component';
+import { PaiementCreateComponent } from './view/paiement/paiement-create/paiement-create.component';
+import { PaiementListComponent } from './view/paiement/paiement-list/paiement-list.component';
 import {FormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
 
